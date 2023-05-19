84758868489f7deea5800dd99c2e6b55c3ec3082
Heartbreaker786 <133631974+Heartbreaker786@users.noreply.github.com>
2023-05-19 09:57:36 +0530

    Add files via upload

---
+++ ShokinMusic/platforms/__init__.py
@@ -0,0 +1,16 @@
+#
+#.
+#
+# This file is part of < https://github.com/Heartbreaker786/ShokinMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/Heartbreaker786/ShokinMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from .Apple import AppleAPI
+from .Carbon import CarbonAPI
+from .Resso import RessoAPI
+from .Soundcloud import SoundAPI
+from .Spotify import SpotifyAPI
+from .Telegram import TeleAPI
+from .Youtube import YouTubeAPI
