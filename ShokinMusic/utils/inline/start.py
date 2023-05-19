8cb9638e176a6085c24dd1bc9856695606afb3b0
Heartbreaker786 <133631974+Heartbreaker786@users.noreply.github.com>
2023-05-19 10:19:36 +0530

    Add files via upload

---
+++ ShokinMusic/utils/inline/start.py
@@ -0,0 +1,19 @@
+#
+# 
+#
+# All rights reserved.
+
+from typing import Union
+
+from pyrogram.types import InlineKeyboardButton
+
+from config import GITHUB_REPO
+from ShokinMusic import app
+
+
+def start_pannel(_):
+    return
+
+
+def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):   
+    return 
